20f7e5324f785305edd42b36a40a064e1a6c12ac
Alina Klishyna <alinakotova102@gmail.com>
2024-04-08 19:21:46 +0200

    Added new title and form display

---
+++ src/main/java/com/gmail/alinakotova102/MainForm.java
@@ -0,0 +1,14 @@
+package com.gmail.alinakotova102;
+
+import javax.swing.*;
+
+public class MainForm extends JFrame{
+    private JPanel panelMain;
+    private JButton select;
+    private JLabel info;
+
+    public MainForm(){
+        super("Invio di messagi dei nuovi clienti");
+        setContentPane(panelMain);
+    }
+}
