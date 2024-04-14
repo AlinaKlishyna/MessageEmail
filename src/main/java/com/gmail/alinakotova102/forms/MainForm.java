4ac91f816131d42646e09cdb467b3d578cb26e15
Alina Klishyna <alinakotova102@gmail.com>
2024-04-14 16:22:28 +0200

    Added new functional for read file

--- src/main/java/com/gmail/alinakotova102/forms/MainForm.java
+++ src/main/java/com/gmail/alinakotova102/forms/MainForm.java
@@ -4,6 +4,12 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import org.apache.poi.hwpf.HWPFDocument;
+import org.apache.poi.hwpf.extractor.WordExtractor;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
 
 public class MainForm extends JFrame{
     private JPanel panelMain;
@@ -20,15 +26,23 @@ public class MainForm extends JFrame{
         //when you press the button..
         select.addActionListener(new ActionListener() {
             @Override
-            public void actionPerformed(ActionEvent e) {
+            public void actionPerformed (ActionEvent e) {
                 //select file
                 JFileChooser openFile = new JFileChooser();
                 //display window dialog
-                int file = openFile.showDialog(null, "Open file");
+                int file = openFile.showDialog(null, "Open");
                 //if file selected.. then
                 if (file == JFileChooser.APPROVE_OPTION){
                     //get selected file
                     File fileEmails = openFile.getSelectedFile();
+
+                    //Read file
+                    FileReader reader = null;
+                    try {
+                        reader = new FileReader(fileEmails);
+                    } catch (FileNotFoundException ex) {
+                        throw new RuntimeException(ex);
+                    }
                 }
             }
         });
